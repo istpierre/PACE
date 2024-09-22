23055e5c6b83cb6c828d11a13fb2fb3195492657
istpierre <40343344+istpierre@users.noreply.github.com>
2024-09-22 13:20:22 -0400

    Update Home.tsx

--- client/src/Screens/Home.tsx
+++ client/src/Screens/Home.tsx
@@ -1,9 +1,13 @@
 import Button from "@mui/material/Button";
 import { useAppContext } from "../Components/contexts";
 import { Typography } from "@mui/material";
+import { useProjects } from "../hooks/useProjects";
+import { useQuery } from "@tanstack/react-query";
 
 export const Home = () => {
   const { login, setLogin } = useAppContext();
+  const { projectsQuery } = useProjects();
+  const projects = useQuery(projectsQuery);
   return (
     <div>
       <div>Home</div>
@@ -20,6 +24,9 @@ export const Home = () => {
       >
         Sign Out
       </Button>
+      {projects?.data ? (
+        <pre>{JSON.stringify(projects.data, null, 2)}</pre>
+      ) : null}
     </div>
   );
 };
